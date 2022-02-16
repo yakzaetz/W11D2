9d9d217a1787dc5978861489018259eaff27dc7e
ian <ianfarr44@gmail.com>
2022-02-16 08:47:32 -0800

    root component with provider

--- todo_app/frontend/todo_redux.js
+++ todo_app/frontend/todo_redux.js
@@ -2,20 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import { receiveTodos, receiveTodo } from "./actions/todo_actions"
+import Root from "./components/root";
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(
-    <div>
-      <h1>Todos App</h1>
-    </div>,
-    
-    root
-    );
-    const store = configureStore();
-    window.store = store;
-    window.receiveTodos = receiveTodos;
+  const store = configureStore();
+  ReactDOM.render(<Root store={store}/>, root);
+
+  
+  window.store = store;
+  window.receiveTodos = receiveTodos;
   });
 
 
